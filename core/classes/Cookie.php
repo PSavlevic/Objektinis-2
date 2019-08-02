8467fb698db6db193ed4c62612f05e5409d35667
Pavel <p.savlevic@gmail.com>
2019-08-02 11:45:34 +0300

    2019.08.02 1 uzd (konstruktorius nusetina $name)

---
+++ core/classes/Cookie.php
@@ -0,0 +1,64 @@
+<?php
+namespace Core;
+
+class Cookie extends Abstracts\Cookie {
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * Turi patikrinti ar cookie duotu pavadinimu egzistuoja
+     */
+    public function exists(): bool
+    {
+        // TODO: Implement exists() method.
+    }
+
+    /**
+     * Turi return'inti json_decode'intą cookie'o
+     * turinį.
+     *
+     * Patikrinti ar pavyko json_decode'inti
+     * (Use Google)
+     * Jei nepavyko, funkcija turi mesti warning'ą
+     * (ne EXCEPTION'ą, bet WARNING'ą - Use Google).
+     * ir return'inti tuščią array
+     *
+     * Jei cookie'is nustatytu pavadinimu neegzistuoja,
+     * turi return'inti tuščią array'ų
+     */
+    public function read(): array
+    {
+        // TODO: Implement read() method.
+    }
+
+    /**
+     * Turi į Cookie duotu pavadinimu
+     * išsaugoti json_encode'intą $data array'jų
+     * (Google setcookie)
+     *
+     * Į cookie galima įrašyt tik string'ą.
+     * Kadangi mes norim galimybę turėti į tą patį
+     * Cookie storinti daugiau data'os, galim tiesiog
+     * encode'inti ir decode'inti array'jų su json'u.
+     *
+     * Mes į cookie įrašysim už'json_encodinę $data
+     * ir atkursim atgal json_decode'inę tai ką radom Cookie
+     *
+     * @param $data array
+     * @param $expires_in int Už kiek laiko sekundemis cookie nebegalios
+     */
+    public function save(array $data, int $expires_in = 3600): void
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * Turi ištrinti Cookie
+     * (Use google)
+     */
+    public function delete(): void
+    {
+        // TODO: Implement delete() method.
+    }
+}
